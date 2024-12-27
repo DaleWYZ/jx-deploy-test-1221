e2cadbc9aaa93b9f8a48ceb7326a01cad9612b5d
wangyz01damddos <wangyz01@damddos.com>
2024-12-27 15:37:44 +0800

    add variables

---
+++ .jx/variables.sh
@@ -0,0 +1,10 @@
+ #!/usr/bin/env bash
+
+# 设置 Docker Registry
+export DOCKER_REGISTRY="harbor.corp.angoo.io"
+export PUSH_CONTAINER_REGISTRY="harbor.corp.angoo.io"
+export DOCKER_REGISTRY_ORG="timelogger"
+
+# 其他变量
+export APP_NAME="timelogger"
+export DOCKERFILE_PATH="Dockerfile" 
