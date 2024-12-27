267b1ffd3d1feeba771f5f5f477ed3b29c1904df
wangyz01damddos <wangyz01@damddos.com>
2024-12-27 16:24:06 +0800

    add version in variables

--- .jx/variables.sh
+++ .jx/variables.sh
@@ -1,4 +1,4 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 # 设置 Docker Registry
 export DOCKER_REGISTRY="harbor.corp.angoo.io"
@@ -7,4 +7,9 @@ export DOCKER_REGISTRY_ORG="timelogger"
 
 # 其他变量
 export APP_NAME="timelogger"
-export DOCKERFILE_PATH="Dockerfile" 
+export DOCKERFILE_PATH="Dockerfile"
+
+# 添加版本号生成规则
+export VERSION=$(date +%Y%m%d)-$(git rev-parse --short HEAD)
+# 或者使用时间戳作为版本号
+# export VERSION=$(date +%Y%m%d.%H%M%S) 
